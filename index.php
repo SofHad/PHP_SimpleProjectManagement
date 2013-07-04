76778dd2e0f151e8ab032292d08a9cb2bd2a404d
Sofiane HADDAG <shaddag@acropolistelecom.net>
2013-07-04 10:03:50 +0200

    Ignore

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   # ------------------------------------------------------------------------
   # SimpleProjectManagement
   # ------------------------------------------------------------------------
-  # Developer : Sofiane Haddag, sofiane.haddag@yahoo.fr
+  # Developer : Sofiane Haddag, sofiane.haddag@yahoo.fr 
  */
 
 header("Location: voir/") ;
+//->
